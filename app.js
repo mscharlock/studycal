508937ea321da720abd8f2fb48fdb45c34a421d4
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-06-15 13:33:33 -0700

    Added third row

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 'use strict';
 
-var daysInMonth = 30;
-var counter = 1;
-var htmlContent = '';
-var dateToday = new Date();
-var month = dateToday.getMonth();
-var day = dateToday.getDate();
-var year = dateToday.getFullYear();
-var headers = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var dates = ['11','12','13', '14', '15', '16', '17'];
+var secondDates = ['18', '19', '20','21', '22', '23', '24'];
+var thirdDates = ['25', '26', '27', '28', '29', '30', '1'];
+var fourthDates = ['2', '3', '4', '5', '6', '7', '8'];
+var headers = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 //Table headers
-var renderTable = function() {
+var renderHeaders = function() {
   var table = document.getElementById('table');
   var getHeaders = document.getElementById('table_header'); //grabbed headers
   var headersArray = [];//made a blank array
@@ -18,8 +15,42 @@ var renderTable = function() {
     headersArray.push('<th>' + headers[i] + '<th>');
   } //cycled through headers to put ths on them
   var headerRow = document.createElement('tr'); //made a new row
-  headerRow.innerHTML = headersArray.join(''); //made the row's content be headersArray
+  getHeaders.innerHTML = headersArray.join(''); //made the row's content be headersArray
   table.appendChild(headerRow); //appended headersRow
 };
 
-renderTable();
+var renderDays = function() {
+  var getDays = document.getElementById('table_days');
+  var daysArray = [];
+  for (var j = 0; j < dates.length; j++) {
+    daysArray.push('<td>' + dates[j] + '<td>');
+  }
+  getDays.innerHTML = daysArray.join('');
+};
+
+var renderDays2 = function() {
+  var table = document.getElementById('table');
+  var getSeconds = document.createElement('tr');
+  var secondsArray = [];
+  for (var k = 0; k < secondDates.length; k++) {
+    secondsArray.push('<td>' + secondDates[k] + '<td>');
+  }
+  getSeconds.innerHTML = secondsArray.join('');
+  table.appendChild(getSeconds);
+};
+
+var renderDays3 = function() {
+  var table = document.getElementById('table');
+  var getThirds = document.createElement('tr');
+  var thirdsArray = [];
+  for (var k = 0; k < thirdDates.length; k++) {
+    thirdsArray.push('<td>' + thirdDates[k] + '<td>');
+  }
+  getThirds.innerHTML = thirdsArray.join('');
+  table.appendChild(getThirds);
+};
+
+renderHeaders();
+renderDays();
+renderDays2();
+renderDays3();
